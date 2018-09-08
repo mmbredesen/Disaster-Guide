76d442faeb2e820f3ebd97136729c388bfe03305
Ali Alkeldi <ali.alkeldi@gmail.com>
2018-09-08 15:21:47 -0400

    demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
-
+import Disaster from './Disaster.js'
+import Data from './Data.js'
+import C3Chart from 'react-c3js';
+import 'c3/c3.css';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.showContent = this.showContent.bind(this)
+    this.state = {
+      currentDisaster: 0,
+      content: '',
+      styles: {
+        app: {
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          height: '100%',
+
+        },
+        leftMenu: {
+          width: '20%',
+          height: '100%',
+          backgroundColor: '#f1f1f1'
+        },
+        content: {
+          width: '80%',
+          height: '100%',
+        },
+      }
+    }
+  }
+
+  showContent = (index, item) => {
+    const data = {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    };
+    this.setState((prevState, props)=>{
+      return{
+        content: <C3Chart data={data} />
+      }
+    });
+  }
+
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <div id='App' style={this.state.styles.app}>
+        <div id='leftMenu' style={this.state.styles.leftMenu}>
+          {
+            Data.map((item, i) => {
+              return <Disaster index={i} key={i} item={item} onClick={this.showContent}/>
+            })
+          }
+        </div>
+        <div id='content' style={this.state.styles.content}>
+          {this.state.content}
+        </div>
+      </div>
     );
   }
 }
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//start init
+document.body.style.margin = '0px'
+var root = document.getElementById('root');
+root.style.height = '100%';
+root.style.width = '100%';
+root.style.padding = '0px'
+//end init
+ReactDOM.render(<App />, root);
